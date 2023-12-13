8a65c1395f9047d5d64df1b7050258286b545065
Hunter Rick <hunter.ellis.rick@gmail.com>
2023-12-12 16:30:00 -0800

    CMAKE environment and googletest setup

---
+++ tests/source/AudioProcessorTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "H_DISTORTION/PluginProcessor.h"
+
+namespace audio_plugin_test
+{
+    TEST(AudioPlugin, foo)
+    {
+        AudioPluginAudioProcessor processor;
+        ASSERT_FALSE(false);
+    }
+}
